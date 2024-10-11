1f35016f8cc23b6e179cb586bdc52aadda2bb98d
soon-yau <soonyau@sooner.ai>
2024-10-11 15:06:44 +0100

    web

---
+++ static/js/index.js
@@ -0,0 +1,28 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 5000,
+    }
+
+		// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+	
+    bulmaSlider.attach();
+
+})
+
+
+function scrollToTop() {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
